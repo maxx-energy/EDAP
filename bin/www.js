5753c15a1c9f6c58a6ec9082f58b759b41bc3d13
esmail sarwari <esmail.sarwari10@gmail.com>
2025-03-19 23:10:22 -0700

    port updated

--- bin/www.js
+++ bin/www.js
@@ -14,7 +14,7 @@ const debug = createDebug('edap:server');
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3002');
 app.set('port', port);
 
 /**
